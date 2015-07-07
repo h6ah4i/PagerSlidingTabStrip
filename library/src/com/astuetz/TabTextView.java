195a74aa045d36d85785d1683d3cc70413c16c43
Haruki Hasegawa <h6a.h4i.0@gmail.com>
2015-07-07 19:47:44 +0900

    Add "Badge" feature
    Specificationt:
     Title <123>
     -> Title: text
     -> 123:   badge (drawn on round rect)

--- library/src/com/astuetz/TabTextView.java
+++ library/src/com/astuetz/TabTextView.java
@@ -8,6 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.text.TextPaint;
 import android.text.TextUtils;
@@ -16,13 +17,23 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TabTextView extends View {
     @SuppressWarnings("unused")
     private static final String TAG = "TabTextView";
 
+    private static final Pattern BADGE_TEXT_PATTERN = Pattern.compile("^(.*)\\s*<(.*)>\\s*$"); // ex.) "Title <123>"
+    private static final float BADGE_TEXT_FONT_SCALE = 0.85f;
+    private static final float BADGE_LEFT_SPACE_DP = 4.0f;
+    private static final float BADGE_TEXT_VERTICAL_PADDING_DP = 2.0f;
+
     private String mText = "";
     private String mRenderText = "";
+    private String mBadgeText;
     private TextPaint mTextPaint;
+    private TextPaint mBadgeTextPaint;
     private int mTextColor;
     private int mCurrentTextColor;
     private ColorStateList mTextColorStateList;
@@ -30,12 +41,19 @@ public class TabTextView extends View {
     private Rect mClipBounds;
     private Rect mGravityRect;
     private Rect mMeasuredBounds;
-    private int mMeasuredLineHeight = -1;
+    private int mMeasuredTextLineHeight = -1;
     private int mMeasuredTextWidth = -1;
-    private Paint.FontMetricsInt mFontMetrics;
+    private int mMeasuredBadgeLineHeight = -1;
+    private int mMeasuredBadgeTextWidth = -1;
+    private Paint.FontMetricsInt mTextFontMetrics;
+    private Paint.FontMetricsInt mBadgeFontMetrics;
     private int mMinWidth = 0;
     private int mMinHeight = 0;
     private boolean mAllCaps = false;
+    private RectF mTempRectF = new RectF();
+    private int mBadgeLeftSpace;
+    private int mBadgeVerticalPadding;
+    private int mBadgeTextColor = Color.WHITE;
 
     public TabTextView(Context context) {
         super(context);
@@ -44,16 +62,22 @@ public class TabTextView extends View {
 
     void init() {
         final Resources res = getResources();
+        float density = res.getDisplayMetrics().density;
 
         mTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.density = res.getDisplayMetrics().density;
+        mTextPaint.density = density;
         mTextPaint.setTextSize(15.0f);
+        mBadgeTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+        mBadgeTextPaint.density = density;
+        mBadgeTextPaint.setTextSize(15.0f * BADGE_TEXT_FONT_SCALE);
         mTextColor = mCurrentTextColor = Color.WHITE;
         mTextColorStateList = null;
         mGravity = Gravity.TOP | Gravity.LEFT;
         mClipBounds = new Rect();
         mGravityRect = new Rect();
         mMeasuredBounds = new Rect();
+        mBadgeLeftSpace = (int) (BADGE_LEFT_SPACE_DP * density);
+        mBadgeVerticalPadding = (int) (BADGE_TEXT_VERTICAL_PADDING_DP * density);
 
         updateTextBounds();
     }
@@ -112,6 +136,8 @@ public class TabTextView extends View {
     private void setRawTextSize(float size) {
         if (size != mTextPaint.getTextSize()) {
             mTextPaint.setTextSize(size);
+            mBadgeTextPaint.setTextSize(size * BADGE_TEXT_FONT_SCALE);
+
             updateTextBounds();
             requestLayout();
             invalidate();
@@ -142,47 +168,114 @@ public class TabTextView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         final String text = mRenderText;
-        final TextPaint paint = mTextPaint;
+        final TextPaint textPaint = mTextPaint;
         final int textWidth = mMeasuredTextWidth;
+        final TextPaint badgePaint = mBadgeTextPaint;
+        final int badgeWidth;
+        final int badgeAndSpaceWidth;
 
-        paint.setColor(mCurrentTextColor);
+        if (mMeasuredBadgeTextWidth >= 0) {
+            badgeWidth = mMeasuredBadgeTextWidth + (mMeasuredBadgeLineHeight + mBadgeVerticalPadding * 2);
+            badgeAndSpaceWidth = badgeWidth + mBadgeLeftSpace;
+        } else {
+            badgeWidth = 0;
+            badgeAndSpaceWidth = 0;
+        }
 
         canvas.getClipBounds(mClipBounds);
 
-        mMeasuredBounds.left = 0;
-        mMeasuredBounds.top = 0;
-        mMeasuredBounds.right = getMeasuredWidth();
-        mMeasuredBounds.bottom = getMeasuredHeight();
-        // mClipBounds.left = 0;
-        // mClipBounds.top = 0;
-        Gravity.apply(
-                mGravity,
-                textWidth, mMeasuredLineHeight,
-                mMeasuredBounds, mGravityRect);
-        Gravity.applyDisplay(mGravity, mMeasuredBounds, mGravityRect);
+        {
+            mMeasuredBounds.left = 0;
+            mMeasuredBounds.top = 0;
+            mMeasuredBounds.right = getMeasuredWidth();
+            mMeasuredBounds.bottom = getMeasuredHeight();
+            // mClipBounds.left = 0;
+            // mClipBounds.top = 0;
+            Gravity.apply(
+                    mGravity,
+                    textWidth + badgeAndSpaceWidth,
+                    mMeasuredTextLineHeight,
+                    mMeasuredBounds, mGravityRect);
+            Gravity.applyDisplay(mGravity, mMeasuredBounds, mGravityRect);
 
-        final Paint.FontMetricsInt fm = getFontMetricsInt();
-        final int textLeft = mGravityRect.left;
-        final int textBottom = mGravityRect.bottom - (fm.descent + fm.leading);
+            final Paint.FontMetricsInt fm = getTextFontMetricsInt();
+            final int textLeft = mGravityRect.left;
+            final int textBottom = mGravityRect.bottom - (fm.descent + fm.leading);
 
-        canvas.drawText(text, textLeft, textBottom, paint);
+            textPaint.setColor(mCurrentTextColor);
+            canvas.drawText(text, textLeft, textBottom, textPaint);
+        }
+
+        if (badgeWidth > 0) {
+            int r = mGravityRect.bottom;
+            RectF rect = mTempRectF;
+
+            int left = mGravityRect.right - badgeWidth;
+            int top = mGravityRect.top - mBadgeVerticalPadding;
+            int bottom = mGravityRect.bottom + mBadgeVerticalPadding;
+
+            rect.set(left, top, left + badgeWidth, bottom);
+
+            canvas.drawRoundRect(rect, r, r, textPaint);
+
+            mMeasuredBounds.left = left;
+            mMeasuredBounds.top = top;
+            mMeasuredBounds.right = left + badgeWidth;
+            mMeasuredBounds.bottom = bottom;
+
+            Gravity.apply(
+                    Gravity.CENTER,
+                    mMeasuredBadgeTextWidth, mMeasuredBadgeLineHeight,
+                    mMeasuredBounds, mGravityRect);
+
+            final Paint.FontMetricsInt fm = getBadgeFontMetricsInt();
+            final int textLeft = mGravityRect.left;
+            final int textBottom = mGravityRect.bottom - (fm.descent + fm.leading);
+
+            badgePaint.setColor(mBadgeTextColor);
+
+            canvas.drawText(mBadgeText, textLeft, textBottom, badgePaint);
+        }
     }
 
-    private Paint.FontMetricsInt getFontMetricsInt() {
-        if (mFontMetrics == null) {
-            mFontMetrics = mTextPaint.getFontMetricsInt();
+    private Paint.FontMetricsInt getTextFontMetricsInt() {
+        if (mTextFontMetrics == null) {
+            mTextFontMetrics = mTextPaint.getFontMetricsInt();
         } else {
-            mTextPaint.getFontMetricsInt(mFontMetrics);
+            mTextPaint.getFontMetricsInt(mTextFontMetrics);
         }
-        return mFontMetrics;
+        return mTextFontMetrics;
+    }
+
+    private Paint.FontMetricsInt getBadgeFontMetricsInt() {
+        if (mBadgeFontMetrics == null) {
+            mBadgeFontMetrics = mBadgeTextPaint.getFontMetricsInt();
+        } else {
+            mBadgeTextPaint.getFontMetricsInt(mBadgeFontMetrics);
+        }
+        return mBadgeFontMetrics;
     }
 
     private void updateTextBounds() {
-        final Paint.FontMetricsInt fm = getFontMetricsInt();
-        final String text = mRenderText;
+        {
+            final Paint paint = mTextPaint;
+            final Paint.FontMetricsInt fm = getTextFontMetricsInt();
+            final String text = mRenderText;
 
-        mMeasuredLineHeight = fm.descent - fm.ascent + fm.leading;
-        mMeasuredTextWidth = (int) Math.ceil(mTextPaint.measureText(text));
+            mMeasuredTextLineHeight = getLineHeight(fm);
+            mMeasuredTextWidth = getTextWidth(paint, text);
+        }
+
+        if (mBadgeText != null) {
+            final Paint paint = mBadgeTextPaint;
+            final Paint.FontMetricsInt fm = getBadgeFontMetricsInt();
+            final String text = mBadgeText;
+
+            mMeasuredBadgeLineHeight = getLineHeight(fm);
+            mMeasuredBadgeTextWidth = getTextWidth(paint, text);
+        } else {
+            mMeasuredBadgeTextWidth = -1;
+        }
     }
 
     public void setGravity(int gravity) {
@@ -198,18 +291,27 @@ public class TabTextView extends View {
         if (mText.equals(text))
             return;
 
-        setText(text, mAllCaps);
-
-        mText = text.toString();
+        setText(text, mAllCaps, true);
 
         updateTextBounds();
         requestLayoutIfNeeded();
         invalidate();
     }
 
-    private void setText(CharSequence text, boolean allCaps) {
+    private void setText(CharSequence text, boolean allCaps, boolean updateBadge) {
+        if (updateBadge) {
+            Matcher m = BADGE_TEXT_PATTERN.matcher(text);
+            if (m.find()) {
+                mText = m.group(1);
+                mBadgeText = m.group(2);
+            } else {
+                mText = text.toString();
+                mBadgeText = null;
+            }
+        } else {
+            mText = text.toString();
+        }
 
-        mText = text.toString();
         mRenderText = (allCaps) ? mText.toUpperCase() : mText;
 
         updateTextBounds();
@@ -227,7 +329,7 @@ public class TabTextView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        if (mMeasuredTextWidth <= 0 || mMeasuredLineHeight <= 0) {
+        if (mMeasuredTextWidth <= 0 || mMeasuredTextLineHeight <= 0) {
             updateTextBounds();
         }
 
@@ -245,6 +347,11 @@ public class TabTextView extends View {
         } else {
             // Measure the text
             result = mMeasuredTextWidth + getPaddingLeft() + getPaddingRight();
+
+            if (mMeasuredBadgeTextWidth >= 0) {
+                int badgeWidth = mMeasuredBadgeTextWidth + (mMeasuredBadgeLineHeight + mBadgeVerticalPadding * 2);
+                result += mBadgeLeftSpace + badgeWidth;
+            }
 
             if (specMode == MeasureSpec.AT_MOST) {
                 // Respect AT_MOST value if that was what is called for by
@@ -269,7 +376,7 @@ public class TabTextView extends View {
             result = specSize;
         } else {
             // Measure the text (beware: ascent is a negative number)
-            result = mMeasuredLineHeight + getPaddingTop() + getPaddingBottom();
+            result = mMeasuredTextLineHeight + getPaddingTop() + getPaddingBottom();
             if (specMode == MeasureSpec.AT_MOST) {
                 // Respect AT_MOST value if that was what is called for by
                 // measureSpec
@@ -313,11 +420,17 @@ public class TabTextView extends View {
             // now compute what (if any) algorithmic styling is needed
             int typefaceStyle = tf != null ? tf.getStyle() : 0;
             int need = style & ~typefaceStyle;
-            mTextPaint.setFakeBoldText((need & Typeface.BOLD) != 0);
-            mTextPaint.setTextSkewX((need & Typeface.ITALIC) != 0 ? -0.25f : 0);
+            boolean fakeBold = (need & Typeface.BOLD) != 0;
+            float skewX = (need & Typeface.ITALIC) != 0 ? -0.25f : 0;
+            mTextPaint.setFakeBoldText(fakeBold);
+            mTextPaint.setTextSkewX(skewX);
+            mBadgeTextPaint.setFakeBoldText(fakeBold);
+            mBadgeTextPaint.setTextSkewX(skewX);
         } else {
             mTextPaint.setFakeBoldText(false);
             mTextPaint.setTextSkewX(0);
+            mBadgeTextPaint.setFakeBoldText(false);
+            mBadgeTextPaint.setTextSkewX(0);
             setTypeface(tf);
         }
     }
@@ -325,6 +438,7 @@ public class TabTextView extends View {
     private void setTypeface(Typeface tf) {
         if (mTextPaint.getTypeface() != tf) {
             mTextPaint.setTypeface(tf);
+            mBadgeTextPaint.setTypeface(tf);
 
             requestLayout();
             invalidate();
@@ -336,7 +450,7 @@ public class TabTextView extends View {
             return;
         }
         mAllCaps = allCaps;
-        setText(mText, mAllCaps);
+        setText(mText, mAllCaps, false);
     }
 
     public void setTextColor(ColorStateList textColor) {
@@ -346,5 +460,13 @@ public class TabTextView extends View {
         mTextColorStateList = textColor;
         applyCurrentDrawableStateTextColor();
         invalidate();
+    }
+
+    private static int getTextWidth(Paint paint, String text) {
+        return (int) Math.ceil(paint.measureText(text));
+    }
+
+    private static int getLineHeight(Paint.FontMetricsInt fm) {
+        return fm.descent - fm.ascent + fm.leading;
     }
 }
